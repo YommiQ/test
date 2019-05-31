a1c7266a860006c17092120c43931f375807f6a9
YommiQ <yommiq@gmail.com>
2019-05-31 19:43:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var number = 5;
+var string = "Hello";
+var symbol = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+let person = {
+    name: "Jhon",
+    age: 25,
+    isMarried: false
+}
+
+console.log(person.age);
+console.log(null == undefined)
+console.log(null === undefined)
+
+let array = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(array[0]);
+console.log(typeof(null));
+console.log(typeof(undefined));
